5b7ca2cf837d0f727c53d8b82dda72a7238edfd0
isayani <isayani95@gmail.com>
2022-10-09 17:57:40 -0400

    removed forgotten comment

--- db/seeds.sql
+++ db/seeds.sql
@@ -21,7 +21,7 @@ VALUES  ("Engineering Manager", 120000, 1),
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES  ("Roxy", "Say",  1, NULL),
-        ("Jane", "Doe",  2, 1), lead
+        ("Jane", "Doe",  2, 1),
         ("Janet", "Doe",  3, 1), 
         ("Jennifer", "Doe",  4, NULL), 
         ("Julia", "Doe",  5, 4),
