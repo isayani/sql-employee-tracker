5897b0d07c8bd07cf4eb98420477a3ea15fe4169
isayani <isayani95@gmail.com>
2022-10-10 23:20:23 -0400

    init switch for all view cases

--- index.js
+++ index.js
@@ -22,6 +22,51 @@ async function dbConnection() {
                 database: dbName,
             }
         );
+
+        // empty variables for query returns and prompt responses
+        let returnedRowsFromDb = [];
+        let returnedOutputFromInq = {};
+
+        // switch for all user input cases
+        switch (select) {
+            case 'View All Departments':
+                returnedRowsFromDb = await db.query('SELECT * FROM department');
+                console.table(returnedRowsFromDb); // needs to be part of array?
+                break;
+
+
+            case 'View All Roles':
+                returnedRowsFromDb = await db.query(`
+                SELECT
+                    role.id,
+                    role.title,
+                    role.salary,
+                    department.name AS department
+                FROM role
+                JOIN department ON role.department_id = department.id
+                `);
+                console.table(returnedRowsFromDb); // needs to be part of array?
+                break;
+
+            case 'View All Employees':
+                returnedRowsFromDb = await db.query(`
+                SELECT
+                    employee.id,
+                    employee.first_name,
+                    employee.last_name,
+                    role.title AS title,
+                    department.name AS department,
+                    role.salary AS salary,
+                    CASE WHEN manager_id IS NOT NULL THEN CONCAT(manager_table.first_name,' ', manager_table.last_name) ELSE NULL END AS manager
+                FROM employee
+                JOIN role ON employee.role_id = role.id
+                JOIN department ON role.department_id = department.id
+                JOIN employee manager_table ON employee.manager_id = manager_table.id
+                `
+                );
+                    
+
+        }
     } catch (err) {
         console.log(err)
     }
